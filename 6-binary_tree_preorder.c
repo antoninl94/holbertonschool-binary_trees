47a44e28cb8cbc1f3560b18b630f9a9f0965551d
antoninl94 <9803@holbertonstudents.com>
2024-12-09 16:38:54 +0100

    binary_tree_preorder : fixing betty error

--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -6,7 +6,7 @@
  * @func: is a pointer to a function to call for each node.
  */
 void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
-{ 
+{
 	if (tree == NULL || func == NULL)
 		return;
 	func(tree->n);
